aa2a1330e02ca8260597992d18ffd0bc7dd60739
kunal-vaishnav <89318333+kunal-vaishnav@users.noreply.github.com>
2025-03-03 22:42:52 +0530

    Update Projects.jsx

--- frontend/src/Projects.jsx
+++ frontend/src/Projects.jsx
@@ -1,12 +1,12 @@
 import FoodOrderImage from "./assets/FoodOrder.png";
 import TicTacToeImage from "./assets/TicTacToe.png";
-import ExpenseManagerImage from "./assets/ExpenseManager.png";
+import PlacepickerImage from "./assets/Placepicker.png";
+
 const Projects = () => (
   <div className="min-h-screen bg-gray-900 text-white flex flex-col items-center justify-center px-16 py-10 w-full">
     <h1 className="text-5xl font-bold mb-12 text-center w-full">PROJECTS</h1>
 
     <div className="space-y-16 w-full max-w-[1600px]">
-      {/* Food Order App */}
       <div className="flex flex-col md:flex-row items-center w-full">
         <div className="md:w-1/2 p-6">
           <h2 className="text-3xl font-semibold">Food Order App</h2>
@@ -25,7 +25,6 @@ const Projects = () => (
         </div>
       </div>
 
-      {/* Tic-Tac-Toe */}
       <div className="flex flex-col md:flex-row-reverse items-center w-full">
         <div className="md:w-1/2 p-6">
           <h2 className="text-3xl font-semibold">Tic-Tac-Toe</h2>
@@ -44,19 +43,20 @@ const Projects = () => (
         </div>
       </div>
 
-      {/* Expense Manager */}
       <div className="flex flex-col md:flex-row items-center w-full">
         <div className="md:w-1/2 p-6">
-          <h2 className="text-3xl font-semibold">Expense Manager</h2>
+          <h2 className="text-3xl font-semibold">Placepicker</h2>
           <p className="text-lg">
-            A web app designed to track and manage daily expenses. Allows users
-            to categorize expenses, view summaries, and gain financial insights.
+            Developed a full-stack Placeker app leveraging geolocation to
+            calculate latitude and longitude, identifying the nearest locations
+            through navigation. Integrated two APIs: one for fetching places and
+            another for user-selected locations.
           </p>
         </div>
         <div className="md:w-1/2 flex justify-center">
           <img
-            src={ExpenseManagerImage}
-            alt="Expense Manager"
+            src={PlacepickerImage}
+            alt="Placepicker"
             className="rounded-lg shadow-lg w-full max-w-[700px]"
           />
         </div>
